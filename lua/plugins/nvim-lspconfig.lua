4aeeab18bb456e4df7b0adb43481bf4c56d60e70
wold9168 <moewold@outlook.com>
2026-09-20 08:04:30 +0800

    disable inlay hints
    Signed-off-by: wold9168 <moewold@outlook.com>

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,8 @@
 return {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-        ---@type lspconfig.options
-        servers = {
-            -- pyright will be automatically installed with mason and loaded with lspconfig
-            pyright = {},
-        },
-    },
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    inlay_hints = { enabled = false },
+  },
 }
